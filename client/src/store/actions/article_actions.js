b4687c052f79b6210fef229f6ffb571ebea8b392
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-11-24 08:48:08 +0000

    Getting articles from Load More

---
+++ client/src/store/actions/article_actions.js
@@ -0,0 +1,17 @@
+import * as articles from './index'
+import axios from 'axios'
+
+axios.defaults.headers.post['Content-Type'] = 'application/jsom'
+
+export const getArticles = (sort) => {
+    return async(dispatch,getState)=>{
+        try{
+            const arts = await axios.post(`/api/articles/loadmore`,sort)
+
+            dispatch(articles.getArticles(arts.data))
+
+        } catch(error){
+
+        }
+    }
+}
